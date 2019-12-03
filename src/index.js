fb6589bbdf76c7d3c34fdd8ab55f1fe95e287126
ltaoo <litaowork@aliyun.com>
2019-12-04 00:42:10 +0800

    feat: 实现旋转 90 度内能正确旋转

--- src/index.js
+++ src/index.js
@@ -4,8 +4,47 @@ class EnhancedCtx {
     }
     drawImage(img, options) {
         const ctx = this.originalCtx;
-        const { x, y } = options;
-        ctx.drawImage(img, x, y);
+        const { x, y, scale, rotate } = options;
+        if (typeof scale === 'number') {
+            ctx.scale(scale, scale);
+        }
+        let lastX = x;
+        let lastY = y;
+        if (typeof rotate === 'number') {
+            ctx.rotate(rotate * Math.PI / 180);
+
+            const hypotenuse = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+            const cos = x / hypotenuse;
+            // 用反三角函数求弧度
+            const radina = Math.acos(cos);
+            // 将弧度转换成角度
+            const angle = Math.floor(180 / (Math.PI / radina));
+
+            console.log('angle of image', angle, rotate);
+            if (angle === rotate) {
+                lastX = hypotenuse;
+                lastY = 0;
+            } else if (angle > rotate) {
+                const totalAngle = angle - rotate;
+                const realX = Math.cos(totalAngle * Math.PI / 180) * hypotenuse;
+                const realY = Math.sin(totalAngle * Math.PI / 180) * hypotenuse;
+
+                console.log(realX, realY);
+                lastX = realX;
+                lastY = realY;
+            } else {
+                const totalAngle = angle + rotate;
+                const realX = Math.sin(totalAngle * Math.PI / 180) * hypotenuse;
+                const realY = Math.cos(totalAngle * Math.PI / 180) * hypotenuse;
+
+                console.log(realX, realY);
+                lastX = realX;
+                lastY = -realY;
+            }
+
+        }
+        ctx.drawImage(img, lastX, lastY);
     }
 }
 
